559094094a8d9cb23ca209bf5aa77448478e2154
zmohamed9 <zainaamohamed12@gmail.com>
2026-04-10 12:04:23 -0600

    Add JSON API Endpoint for About portion

--- src/pages/api/about.js
+++ src/pages/api/about.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({
+    about: {
+      title: "About Me",
+      intro: "Hi, I'm Zaina, a designer based in Alberta.",
+      toolsIntro: "My primary tools of choice includes:",
+      tools: ["Figma", "HTML", "CSS"],
+      passion: "Beyond designing, I enjoy hanging out with my pets and loved ones. I enjoy playing boardgames and trying various coffee/matcha.",
+      approach: "I want to understand the client's needs as well as the user pain points, before I start designing a rough sketch of the digital product."
+    }
+  });
 }
